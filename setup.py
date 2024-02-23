df94377570eaa5ce59c35324a0ebacdde95da41e
Siva-0310 <sivachadalawada0310.email@example.com>
2024-02-23 22:44:53 +0530

    added git ignore

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 from setuptools import setup
+
+setup(
+    name='clip',
+    version='1.0.0',
+    py_modules=['clip'],
+    entry_points={
+        'console_scripts': [
+            'clip = clip:main'
+        ]
+    }
+)
